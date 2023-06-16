4765522fc8e4c0a0ac6edccdec2c6868b1b7892f
saimonh3 <saimon2721@gmail.com>
2023-06-15 22:48:33 -0400

    format code and add readme.txt

--- readme.md
+++ readme.md
@@ -1,5 +1,3 @@
-# TK URL Alias
-
-TK URL Alias allows you to easily create custom, user-friendly URLs for your posts and pages. Enhance the accessibility and SEO of your website by specifying alternative URLs that are descriptive, memorable, and tailored to your content.
-
-With a simple interface, TK URL Alias empowers you to optimize your URL structure, improve navigation, and provide a better user experience.
+# URL Alias
+URL Alias allows you to easily create custom, user-friendly URLs for your posts and pages. Enhance the accessibility and SEO of your website by specifying alternative URLs that are descriptive, memorable, and tailored to your content.
+With a simple interface, URL Alias empowers you to optimize your URL structure, improve navigation, and provide a better user experience.
--- src/url-alias.js
+++ src/url-alias.js
@@ -1,41 +1,48 @@
 const { __ } = wp.i18n;
-const {compose} = wp.compose;
-const {withSelect, withDispatch} = wp.data;
-const {TextControl, PanelRow} = wp.components;
-const {PluginDocumentSettingPanel} = wp.editPost;
+const { compose } = wp.compose;
+const { withSelect, withDispatch } = wp.data;
+const { TextControl, PanelRow } = wp.components;
+const { PluginDocumentSettingPanel } = wp.editPost;
 
-const UrlAlias = ({postType, postMeta, setPostMeta}) => {
-	if (!window?.tkUrlAlias?.postTypes?.includes(postType)) {
+const UrlAlias = ( { postType, postMeta, setPostMeta } ) => {
+	if ( ! window?.tkUrlAlias?.postTypes?.includes( postType ) ) {
 		return;
 	}
 
 	return (
-		<PluginDocumentSettingPanel name="tk-url-alias" title="URL Alias" className="tk-url-alias">
+		<PluginDocumentSettingPanel
+			name="tk-url-alias"
+			title="URL Alias"
+			className="tk-url-alias"
+		>
 			<PanelRow>
 				<TextControl
-					value={postMeta?.tk_url_alias}
-					onChange={(value) => {
-						setPostMeta({tk_url_alias: value});
-					}}
-					help={ __( 'Specify an alternative path for your posts and pages', 'tk-url-alias' ) }
+					value={ postMeta?.tk_url_alias }
+					onChange={ ( value ) => {
+						setPostMeta( { tk_url_alias: value } );
+					} }
+					help={ __(
+						'Specify an alternative path for your posts and pages',
+						'tk-url-alias'
+					) }
 				/>
 			</PanelRow>
 		</PluginDocumentSettingPanel>
 	);
 };
 
-export default compose([
-	withSelect((select) => {
+export default compose( [
+	withSelect( ( select ) => {
 		return {
-			postMeta: select('core/editor').getEditedPostAttribute('meta'),
-			postType: select('core/editor').getCurrentPostType(),
+			postMeta: select( 'core/editor' ).getEditedPostAttribute( 'meta' ),
+			postType: select( 'core/editor' ).getCurrentPostType(),
 		};
-	}),
-	withDispatch((dispatch) => {
+	} ),
+	withDispatch( ( dispatch ) => {
 		return {
-			setPostMeta(newMeta) {
-				dispatch('core/editor').editPost({meta: newMeta});
-			}
+			setPostMeta( newMeta ) {
+				dispatch( 'core/editor' ).editPost( { meta: newMeta } );
+			},
 		};
-	})
-])(UrlAlias);
+	} ),
+] )( UrlAlias );
